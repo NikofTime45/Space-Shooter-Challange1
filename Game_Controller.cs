fc761b36c36b79caf8adeaad7e9f90beea0b7ed8
NikofTime45 <47070043+NikofTime45@users.noreply.github.com>
2019-04-04 13:00:57 -0400

    Scripts

---
+++ Game_Controller.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Game_Controller : MonoBehaviour
+{
+    public GameObject[] hazards;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+    public Text scoreText;
+    public Text restartText;
+    public Text gameOverText;
+
+    private int score;
+    private bool gameOver;
+    private bool restart;
+
+    void Start()
+    {
+        StartCoroutine(SpawnWaves());
+        score = 0;
+        UpdateScore();
+        gameOver = false;
+        restart = false;
+        restartText.text = "";
+        gameOverText.text = "";
+     }
+
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+        if (restart)
+        {
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+                SceneManager.LoadScene("SpaceShooterScene");
+            }
+        }
+    }
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                GameObject hazard = hazards[Random.Range(0, hazards.Length)];
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+            if (gameOver)
+            {
+                restartText.text = "Press 'Z' for Restart";
+                restart = true;
+                break;
+            }
+        }
+    }
+
+    public void AddScore(int NewScoreValue)
+    {
+        score += NewScoreValue;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        scoreText.text = "Points: " + score;
+        if (score >= 100)
+        {
+            gameOverText.text = "Game Created by Nicolas Ruiz";
+            gameOver = true;
+            restart = true;
+        }
+    }
+
+    public void GameOver()
+    {
+        gameOverText.text = "Game Over!";
+        gameOver = true;
+    }
+}
